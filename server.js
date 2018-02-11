4bdc1798a07acfef41fe129852d3b33c2e9c6321
fanuch <afanucchi@encoo.com.au>
2018-02-11 13:12:40 +1100

    Added live git log tracker

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const eol = require('eol');
 const app = express()
+var fs = require('fs'),
+    path = require('path'),    
+    filePath = path.join(__dirname, '.git/logs/HEAD');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
+
+
+
 app.get('/', function (req, res) {
-  res.render('index')
+	// rMake the array empty
+	var git_array = [];
+	// Import the Git log
+	fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+    if (!err) {
+    	// Turn the commit lines into json objects
+    	let lines = eol.split(data)
+			lines.forEach(function(line) {
+				if (line.indexOf("commit") !== -1)
+				{
+					git_array.push(line.substring(line.indexOf(":") + 2))
+				}
+		})
+		// Send to web page
+		res.render('index', {
+	  	git: git_array,
+	  })
+    } else {
+          console.log('ERROR');
+    }
+})
 })
 
-//app.post('/', function (req, res) {
-//  console.log(req.body.city);
-// res.render('index')
-//})
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
